e6d1b77b45abf9bcf9e4e6f08e435f480c76b2aa
Yousef-harmouni-4real <96540829+Yousef-harmouni-4real@users.noreply.github.com>
2022-08-06 22:04:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Cookies from 'js.cookie.mjs'
+import Cookies from '/js.cookie.mjs'
 notification_status = Cookies.get("Notification_Permission")
 if (notification_status == "granted" || notification_status == "denined") {
 	console.log("done")
