f07945e47783cf74b027e6320d59739d8424715a
Yousef-harmouni-4real <96540829+Yousef-harmouni-4real@users.noreply.github.com>
2022-08-07 12:39:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -122,6 +122,23 @@ async function Permission() {
 
 		}
 
-if(Notification.permission == "granted") {
-	const greeting = new Notification('Hi, How are you?');
+
+function subscribeUserToPush() {
+  return navigator.serviceWorker.register('script.js')
+  .then(function(registration) {
+    var subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: btoa(
+        'BEl62iUYgUivxIkv69yViEuiBIa-Ib9-SkvMeAtA3LFgDzkrxZJjSgSnfckjBJuBkr3qBUYIHBQFLXYp5Nksh8U'
+      )
+    };
+
+    return registration.pushManager.subscribe(subscribeOptions);
+  })
+  .then(function(pushSubscription) {
+    console.log('PushSubscription: ', JSON.stringify(pushSubscription));
+    return pushSubscription;
+  });
 }
+
+subscribeUserToPush()
