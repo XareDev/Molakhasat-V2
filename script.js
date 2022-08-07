29c18945b4c0312750d269623c91c6959a6dc6e1
Yousef-harmouni-4real <96540829+Yousef-harmouni-4real@users.noreply.github.com>
2022-08-07 14:10:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -121,6 +121,8 @@ async function Permission() {
 
 		}
 
+
+function subscribeUserToPush() {
 	navigator.serviceWorker.register('service-worker.js').then(
   function(serviceWorkerRegistration) {
   	serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true, applicationServerKey:
@@ -141,3 +143,17 @@ async function Permission() {
       }
     );
   });
+}
+
+navigator.serviceWorker.register('service-worker.js');
+
+if (notification_status === 'granted') {
+  navigator.serviceWorker.ready.then(function(registration) {
+    registration.showNotification('Vibration Sample', {
+      body: 'Buzz! Buzz!',
+      icon: '../images/touch/chrome-touch-icon-192x192.png',
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+      tag: 'vibration-sample'
+    });
+  });
+}
