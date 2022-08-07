9cb91670222cee0c38c4ee0aa56619e7f7896e1e
Yousef-harmouni-4real <96540829+Yousef-harmouni-4real@users.noreply.github.com>
2022-08-07 15:38:57 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,15 @@ self.addEventListener('notificationclick', function(event) {
     let url = 'https://molakhasat.netlify.app';
     event.notification.close(); // Android needs explicit close.
     event.waitUntil(
-        clients.matchAll({type: 'window'}).then( windowClients => {
+        clients.matchAll({includeUncontrolled: true}).then( windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
             // If not, then open the target URL in a new window/tab.
             if (clients.openWindow) {
                 return clients.openWindow(url);
@@ -15,5 +23,6 @@ self.addEventListener('notificationclick', function(event) {
       body: 'تمت إضافة ملخصات جديدة!',
       icon: 'logo.png',
       vibrate: [200, 100, 200, 100, 200, 100, 200],
-    })
-
+      tag: 'New-Molakhasat',
+      actions: [{action: "open_url", title: "رؤية المزيد"}]
+    });
