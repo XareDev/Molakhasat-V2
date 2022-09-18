e4902ccd49930bba6f53fb78bc415474b562b1df
Youcef Harmouni <xaredev@gmail.com>
2022-09-18 03:13:28 +0100

    solved maybe

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 self.addEventListener('notificationclick', function(event) {
     let url = 'https://molakhasat.netlify.app/chart.html';
     event.notification.close(); // Android needs explicit close.
@@ -33,43 +31,3 @@ self.registration.sendNotification('ملخصات جديدة', {
 
 
     
-=======
-=======
->>>>>>> e0f48fdd7b735841e347ec77f541b7917ed2b613
-self.addEventListener('notificationclick', function(event) {
-    let url = 'https://molakhasat.netlify.app/chart.html';
-    event.notification.close(); // Android needs explicit close.
-    event.waitUntil(
-        clients.matchAll({includeUncontrolled: true}).then( windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                // If so, just focus it.
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
-});
-
-self.registration.sendNotification('ملخصات جديدة', {
-  body: 'تمت إضافة أداة راسم الدالة!',
-  icon: 'logo.png',
-  gcmAPIKey:"AAAAowSf5fQ:APA91bEjr_0pW7E7GL956y9EgqYsCYqonraPOaYGbCsqZjRV3eNBMxc3P8OkFSoQpEMzZ8PKcnMYSyavdj5QC_UWe-lWozCegU6ekMKWObL-08I0gN71vkwsMPTwF1Yc_U1Tkn6MImqD",
-  vibrate: [200, 100, 200, 100, 200, 100, 200],
-  tag: 'New-Molakhasat',
-  actions: [{action: "open_url", title: "رؤية المزيد"}]
-})
-
-
-
-    
-<<<<<<< HEAD
->>>>>>> e0f48fd (initial commit)
-=======
->>>>>>> e0f48fdd7b735841e347ec77f541b7917ed2b613
